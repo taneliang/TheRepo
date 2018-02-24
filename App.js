aa00f3c7e7549b6c24fb9a01c0f4a4af32aaa2b4
E-Liang Tan <eliang@eliangtan.com>
2018-02-24 19:38:31 +0800

    Basic app render

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @flow
 
-export default class App extends React.Component {
-  render() {
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type State = {
+  modeSet: boolean,
+  isDing: ?boolean
+};
+
+export default class App extends React.Component<State> {
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      modeSet: false
+    };
+  }
+
+  renderNotSet() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Not set</Text>
       </View>
     );
+  }
+
+  renderDingDong() {
+    return (
+      <View style={styles.container}>
+        <Text>Ha set</Text>
+      </View>
+    );
+  }
+
+  render() {
+    if (this.state.modeSet) {
+      return this.renderDingDong();
+    }
+    return this.renderNotSet();
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
